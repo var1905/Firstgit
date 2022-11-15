d630d6ee8f89531f5e343f9ca96f42ca25e358b8
var1905 <118063497+var1905@users.noreply.github.com>
2022-11-15 15:09:50 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,5 @@
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000'
+var additems=document.getElementById('addItem')
+additems.style.color="green"
+additems.style.fontWeight='bold'
